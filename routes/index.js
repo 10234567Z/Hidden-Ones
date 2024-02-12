1f3d3f410ffc0eb1ab26de40535e41671a291d8e
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-02-12 20:21:33 +0530

    Let signing up member join club

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require("../models/user")
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-
+const app = express()
 router.get('/', (req, res, next) => {
     res.render("index")
 })
@@ -13,13 +13,38 @@ router.get('/sign-up', (req, res, next) => {
     res.render("signup")
 })
 
+router.get('/join-club' , (req , res , next) => {
+    res.render("join_club", { user: app.get("CurrentUser")})
+})
+
+router.post('/join-club' , [
+    body('clubPass').custom(async (value) => {
+        if(value !== "deez nuts"){
+            throw new Error("Incorrect club password , Entry denied. Login with this account below and join club with correct password")
+        }
+    }),
+    asyncHandler(async (req , res , next) => {
+        const error = validationResult(req)
+        if (!error.isEmpty()) {
+            const user = app.get('CurrentUser')
+            await user.save()
+            return res.render("error", { msg: error.errors[0].msg })
+        }
+        const user = app.get('CurrentUser')
+        user.isMember = true;
+        await user.save()
+        app.set('CurrentUser' , {})
+        res.render('success')
+    })
+])
+
 router.post('/sign-up', [
     body('cpass').custom(async (value, { req }) => {
         if (value != req.body.pass) {
             throw new Error("Password confirmation does not match password");
         }
     }),
-    body('uname').custom(async (value, { req }) => {
+    body('uname').custom(async (value) => {
         let eUser = await User.findOne({ userName: value })
         if (eUser) {
             throw new Error('Username already exists')
@@ -28,7 +53,7 @@ router.post('/sign-up', [
     asyncHandler(async (req, res, next) => {
         const error = validationResult(req)
         if (!error.isEmpty()) {
-            return res.render("error" , { msg: error.errors[0].msg})
+            return res.render("error", { msg: error.errors[0].msg })
         }
         bcrypt.hash(req.body.pass, 10, async (err, hashedPassword) => {
             if (err) {
@@ -43,13 +68,15 @@ router.post('/sign-up', [
                     hash: hashedPassword,
                     isAdmin: req.body.isAdmin,
                 })
-                await user.save()
-                res.redirect('/')
+                app.set("CurrentUser" , user)
+                console.log(req.body.isAdmin)
+                res.redirect('/join-club')
             }
         })
 
     })
 ]
 )
+
 
 module.exports = router;
